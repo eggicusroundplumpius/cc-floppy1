85c51cb1435679d84943bf68495575512bdd7e09
Eggicus Roundplumpius <jaike3@gmail.com>
2020-02-24 11:53:22 +0000

    Comm Center B Final Alpha Test

--- commcenter_b.lua
+++ commcenter_b.lua
@@ -2,40 +2,79 @@
 --Wired modem on back. Part B of Comm Center Package.
 
 --Functions
-function ping (host)
+function listen ()
+	local senderID, message, protocol = rednet.recieve(protocol)
+	local part1, part2, currentpart
+	for i in message do
+		if i == ":" then
+			currentpart = 2
+		elseif currentpart == 1 then
+			part1 = part1 + i
+		elseif currentpart == 2 then
+			part2 = part2 + i
+		end
+	end
+	--Returns message task, message content, sender ID
+	return part1, part2, senderID
+end
+function lookup (host)
     local result = rednet.lookup(protocol, host)
     if result then
+		--Returns Computer ID from lookup and a boolean to represent success
         return result, true
     else
-        return nil
+        return nil, false
     end
 end
-function addProtocol (p)
-    rednet.host(p, hostname)
-    print(p, "protocol now hosting")
+function ping (hostID, kind, timeout)
+	local message = "ping:" + kind
+	rednet.send(hostID, message, protocol)
+	if timeout then
+		local senderID, reply, reply_protocol = rednet.recieve(protocol, timeout)
+	else
+		local senderID, reply, reply_protocol = rednet.recieve(protocol, 5)
+	end
+	while true do
+		local part1, part2, currentpart
+		for i in reply do
+			if i == ":" then
+				currentpart = 2
+			elseif currentpart == 1 then
+				part1 = part1 + i
+			elseif currentpart == 2 then
+				part2 = part2 + i
+			end
+		end
+		if part1 == "ping" then
+			break
+		end
+	end
+	--Returns Computer ID, reply and ping truth
+	if part1 then
+		return senderID, part2, true
+	else
+		return nil, nil, false
+	end
 end
-function removeProtocol (p)
-    rednet.unhost(p, hostname)
-    print(p, "protocol discontinued")
+function establish (site)
+	local chatname = protocol+"/"+hostname+"-"+site
+	shell.run("chat host", chatname, hostname)
+	intralink.transmit(intralink_channel, intralink_channel, chatname)
+end
+function forwardMode ()
+	while true do
+		local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
+		if message == "intralink:disconnect" and modemSide == "back" then
+			break
+		else
+			wireless.transmit(senderChannel, replyChannel, message)
+		end
+	end
+	os.reboot()
 end
 function switchProtocol (p)
     protocol = p
     print("default protocol is now", protocol)
-end
-function establish (s)
-    local id, result = ping(s)
-    if result then
-        print("conection to", s, "success!")
-        local chatname = protocol+"/"+hostname+"-"+s
-        os.run("chat host", chatname, hostname)
-		intralink.transmit(intralin_channel, intralink_channel, chatname)
-	end
-end
-function listen ()
-	local senderID, message, protocol = rednet.recieve(protocol)
-	
-	if message
-	
 end
 
 --Detect and load Config file
@@ -68,7 +107,7 @@ else
     print("hostname:", hostname)
     protocol = "new_link"
     print("default protocol:", protocol)
-    interlink_channel = 1
+    intralink_channel = 1
     print("intralink channel:", intralink_channel)
     
     print("  ")
@@ -130,8 +169,79 @@ sleep(1)
 term.clear()
 term.setCursorPos(0,0)
 
-print("---------------------------")
+print("---------------------------------------------------")
 print("Pathos III Comm Transmitter")
-print("---------------------------")
+print("---------------------------------------------------")
 
+while true do
+	task, message, ID = listen()
+	print("["+ID+"]", task, "'"+message+"'")
+	--Task List for Intralink Commands
+	if source == "intralink" then
+		
+		--Establish Local-Remote Chat
+		if task == "establish" then
+			ID, valid = lookup(message)
+			if valid then
+				ID, message, truth = ping(ID, 0, 5)
+				if truth then
+					print("["+task+"]", ID+"/"+message)
+					establish(mesaage)
+				else
+					print("[system] ping timeout, target comm must be offline")
+				end
+			else
+				print("[system] lookup failed, target comm may not exist")
+			end
+		end
+		
+		--Realign Comm to New Protocol
+		if task == "switch_protocol" then
+			switchProtocol(message)
+		end
+		
+		--Send Ping Command
+		if task == "ping" then
+			ID, truth = lookup(message)
+			replyID, content, valid1 = ping(ID, 0, 5)
+			if valid1 then
+				intralink.transmit(intralink_channel, intralink_channel, replyID+":"+content)
+			else
+				intralink.transmit(intralink_channel, intralink_channel, false)
+			end
+		end
+		
+		--Lookup ID
+		if task == "lookup" then
+			ID, trueness = lookup(message)
+			if trueness then
+				intralink.transmit(intralink_channel, intralink_channel, ID)
+			else
+				intralink.transmit(intralink_channel, intralink_channel, false)
+			end
+		end
+		
+	--Task List for Remote Commands
+	elseif source == "wireless" then
+	
+		--Connect to Remote-Local Chat
+		if task == "chat" then
+			intralink.transmit(intralink_channel, intralink_channel, message)
+			forwardMode()
+		end
+		
+		--Ping Reply
+		if task == "ping" then
+			if message == "0" then
+				rednet.send(ID, "ping:hello_world", broadcast)
+			elseif message == "1" then
+				print("[system] no reply to ping, type 1")
+			elseif message == "2" then
+				print("[system] ping type 2, sending intralink notif")
+				intralink.transmit(intralink_channel, intralink_channel, "ping:"+ID)
+				rednet.send(ID, "ping:hello_world", ID)
+			end
+		end
 
+	end
+end
